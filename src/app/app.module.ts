8bf9c4754b925dc781928937085aa7219618f9f8
Juan Manuel <juan.15121001@gmail.com>
2018-02-05 19:07:38 -0500

    bases para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './pages/home/carousel/carousel.component';
+import { NewsComponent } from './pages/home/news/news.component';
+import { BestSellerComponent } from './pages/home/best-seller/best-seller.component';
+import { HighlightsComponent } from './pages/home/highlights/highlights.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    NewsComponent,
+    BestSellerComponent,
+    HighlightsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
